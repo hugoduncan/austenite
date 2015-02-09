7182749910060dd0c6da5f50c06937e9b52b22b3
Hugo Duncan <hugo@hugoduncan.org>
2015-02-09 12:58:12 -0500

    Initial doc commit

---
+++ implementors/hyper/header/trait.Header.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors['hyper'] = [];
+implementors['austenite'] = ["<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='hyper/header/trait.Header.html' title='hyper::header::Header'>Header</a> for <a class='struct' href='austenite/hyper_headers/struct.AcceptLanguage.html' title='austenite::hyper_headers::AcceptLanguage'>AcceptLanguage</a>","<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='hyper/header/trait.Header.html' title='hyper::header::Header'>Header</a> for <a class='struct' href='austenite/hyper_headers/struct.AcceptCharset.html' title='austenite::hyper_headers::AcceptCharset'>AcceptCharset</a>","<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='hyper/header/trait.Header.html' title='hyper::header::Header'>Header</a> for <a class='struct' href='austenite/hyper_headers/struct.IfMatch.html' title='austenite::hyper_headers::IfMatch'>IfMatch</a>","<a class='stability Unmarked' title='No stability level'></a>impl <a class='trait' href='hyper/header/trait.Header.html' title='hyper::header::Header'>Header</a> for <a class='struct' href='austenite/hyper_headers/struct.IfNoneMatch.html' title='austenite::hyper_headers::IfNoneMatch'>IfNoneMatch</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
